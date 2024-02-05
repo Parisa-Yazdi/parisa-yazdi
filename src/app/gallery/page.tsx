c3173afeb7c7498ab417da257a17895f9823164b
Ziad-GPT <Ziad-GPT@Murads-MacBook-Pro.local>
2024-02-04 21:06:05 -0800

    adding gallery

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import { GalleryComp } from '@/components/gallery-comp';
+
+import React from 'react';
+
+export default function page() {
+  return (
+    <div>
+      <GalleryComp />
+    </div>
+  );
+}
