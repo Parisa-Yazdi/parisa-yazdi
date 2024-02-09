c07f3e02fc011b6c8ca3276bd43c226d43cbfdfd
Murad Qumizakis <67524607+murad-qumizakis@users.noreply.github.com>
2024-02-08 20:45:45 -0800

    only /book is left

--- src/app/book/[bookName]/page.tsx
+++ src/app/book/[bookName]/page.tsx
@@ -2,8 +2,8 @@ import { SingleBook } from '@/components/single-book';
 import { fetchBookBySlug } from '@/lib/utils';
 
 export default async function Book({ params }: any) {
-  const book = await fetchBookBySlug(params.bookName);
-  console.log('book', book.data[0]);
+  // const book = await fetchBookBySlug(params.bookName);
+  // console.log('book', book.data[0]);
 
   const bookName = params.bookName
     .replace(/and/g, '&')
@@ -15,13 +15,13 @@ export default async function Book({ params }: any) {
   return (
     <>
       <main className="mx-auto mb-20  w-7/12 ">
-        <SingleBook
+        {/* <SingleBook
           image={book.data[0].attributes.bookCover.data.attributes.url}
           title={bookName}
           author={book.data[0].attributes.author}
           summary={book.data[0].attributes.description}
           link={book.data[0].attributes.link}
-        />
+        /> */}
       </main>
     </>
   );
